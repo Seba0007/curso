cc610cbedd93176233cd46fb9b01f011a2bc894d
Sebastian Vizcaino <golf001@yahoo.com>
2022-04-25 21:06:49 -0300

    boostrap cards and form added

---
+++ app.js
@@ -0,0 +1,25 @@
+window.onload = function(){
+
+    const correo = document.getElementById("forma");
+    const input = document.getElementById("inputEmail4")
+
+
+
+    boton.addEventListener('click', () => {
+    const boton = document.getElementById("boton");
+    boton.classList.toggle("btn");
+    
+    }) 
+
+    correo.addEventListener('submit', (e) => {
+
+        e.preventDefault();
+        input.Value.trim()
+
+
+    })
+
+    
+
+
+}
